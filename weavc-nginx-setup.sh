9c40c613afe740334671c2aa352041cf6302b8b9
chrisweaver1 <chris.weaver.work@gmail.com>
2020-08-25 13:57:49 +0000

    alpine version

--- weavc-nginx-setup.sh
+++ weavc-nginx-setup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# set default key size
+if [[ -z "$KEY_SIZE" ]]; then
+    KEY_SIZE="2048"
+fi
+    
 # generate ssl certs if non are pre-generated or passed in as volumes
-if [ ! -f "/certs/fullchain.pem" ] || [ ! -f "/certs/privkey.pem" ]; then
-    openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout /defaults/certs/privkey.pem -out /defaults/certs/fullchain.pem -subj /C=$SSL_C/ST=$SSL_ST/L=$SSL_L/O=$SSL_O/OU=$SSL_OU/CN=$SSL_CN
+if [[ ! -v "$SSL_GENERATE" ]] && [ "$SSL_GENERATE" == "yes" ]; then
+    openssl req -newkey rsa:$KEY_SIZE -new -nodes -x509 -days 3650 -keyout /defaults/certs/privkey.pem -out /defaults/certs/fullchain.pem -subj /C=$SSL_C/ST=$SSL_ST/L=$SSL_L/O=$SSL_O/OU=$SSL_OU/CN=$SSL_CN
 fi
 
 # generate dhparam if non is provided
 # helps protect requests if someone was to get ahold of the private key
 # by adding another layer of encryption
-if [ ! -f "/certs/dhparam.pem" ]; then
-    openssl dhparam -out /defaults/certs/dhparam.pem 2048
+if [[ ! -v "$DH_GENERATE" ]] && [ "$DH_GENERATE" == "yes" ]; then
+    openssl dhparam -out /defaults/certs/dhparam.pem $KEY_SIZE
 fi
 
 # copy defaults ro root directory if they dont exist
 # allows data to persist after bind mounts
 cp -r -n /defaults/* /
-
